3112d9daaf43b48cf93094dfbe546cf63f059e77
Sharcz <marc.boulet@shaw.ca>
2017-01-21 16:35:41 -0700

    Programming Assignment 2
    the completed code for the Coursera R Programming Week 3 programming
    assignment. This code is used to compute and store the inverse of a
    matrix in a cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Johns Hopkins University R Programming Week 3 Peer Graded Assignment:
+## Programming Assignment 2: Lexical Scoping (2017-01-21)
+##
+## This assignment involves creating a special matrix object, via the first
+## function, makeCacheMatrix), that can cache its inverse. The second 
+## function, cacheSolve, computes the inverse of the matrix. If the inverse
+## has already been calculated, then the function will retrieve the inverse
+## from the cache.
 
-## Write a short comment describing this function
+## Creates a special matrix object that can cache its inverse using the 
+## lexical scoping rules in R.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+          x <<- y
+          i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Computes the matrix inverse from the function makeCacheMatrix. Retrieves the 
+## inverse from the cache if the inverse has already been calculated from the
+## existing matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+              message("getting cached data")
+              return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
